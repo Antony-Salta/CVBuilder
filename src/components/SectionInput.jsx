2f5b9a44d82138c1f69242cae43c4e2aa7f7f24c
Antony <antony.a.salta@gmail.com>
2024-07-15 11:59:15 +0100

    Made the text areas for section titles smaller

--- src/components/SectionInput.jsx
+++ src/components/SectionInput.jsx
@@ -4,9 +4,9 @@ export default function SectionInput({ id, handleInput, title = null, needsName 
   {
       return(
           <div>
-          {needsName === true ? <> <h3>Section Title</h3> <textarea name = "Section title"onChange={(event) => handleInput(id, false, event)} /></> : <h2>{title}</h2>}
+          {needsName === true ? <> <h3>Section Title</h3> <textarea name = "Section title" className="titleInput" onChange={(event) => handleInput(id, false, event)} /></> : <h2>{title}</h2>}
           {needsName === true && <p>Section Content</p>}
-          <textarea name = "Section Content" onChange={(event) => handleInput(id, true, event)}/>
+          <textarea name = "Section Content" className={needsName === false ? "titleInput" : ""} onChange={(event) => handleInput(id, true, event)}/>
           <br></br>
           <SectionButtons id={id} AddSection={AddSection} DeleteSection={DeleteSection} />
           <br></br>
